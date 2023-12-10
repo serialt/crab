c95ba333654b4353254ef455c968efffa4cf34df
serialt <tserialt@gmail.com>
2023-12-10 23:56:43 +0800

    feat: add dir size

--- file.go
+++ file.go
@@ -673,3 +673,14 @@ Redirect:
 	}
 	return os.Rename(src, dst)
 }
+
+func DirSize(path string) (int64, error) {
+	var size int64
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			size += info.Size()
+		}
+		return err
+	})
+	return size, err
+}
