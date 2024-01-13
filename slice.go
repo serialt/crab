e225c69d3fd3f49b0d7a5388d41dacb61c02104d
serialt <tserialt@gmail.com>
2024-01-13 19:31:58 +0800

    feat: add slice

---
+++ slice.go
@@ -0,0 +1,144 @@
+package crab
+
+import (
+	"slices"
+)
+
+// ContainSubSlice check if the slice contain a given subslice or not.
+// Play: https://go.dev/play/p/bcuQ3UT6Sev
+func ContainSubSlice[T comparable](slice, subSlice []T) bool {
+	for _, v := range subSlice {
+
+		if !slices.Contains(slice, v) {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Chunk creates a slice of elements split into groups the length of size.
+// Play: https://go.dev/play/p/b4Pou5j2L_C
+func SliceChunk[T any](slice []T, size int) [][]T {
+	result := [][]T{}
+
+	if len(slice) == 0 || size <= 0 {
+		return result
+	}
+
+	for _, item := range slice {
+		l := len(result)
+		if l == 0 || len(result[l-1]) == size {
+			result = append(result, []T{})
+			l++
+		}
+
+		result[l-1] = append(result[l-1], item)
+	}
+
+	return result
+}
+
+// Equal checks if two slices are equal: the same length and all elements' order and value are equal.
+// Play: https://go.dev/play/p/WcRQJ37ifPa
+func Equal[T comparable](slice1, slice2 []T) bool {
+	if len(slice1) != len(slice2) {
+		return false
+	}
+
+	for i := range slice1 {
+		if slice1[i] != slice2[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// UpdateAt update the slice element at index.
+// Play: https://go.dev/play/p/f3mh2KloWVm
+func SliceUpdateAt[T any](slice []T, index int, value T) []T {
+	size := len(slice)
+
+	if index < 0 || index >= size {
+		return slice
+	}
+	slice = append(slice[:index], append([]T{value}, slice[index+1:]...)...)
+
+	return slice
+}
+
+// Unique remove duplicate elements in slice.
+// Play: https://go.dev/play/p/AXw0R3ZTE6a
+func SliceUnique[T comparable](slice []T) []T {
+	result := []T{}
+
+	for i := 0; i < len(slice); i++ {
+		v := slice[i]
+		skip := true
+		for j := range result {
+			if v == result[j] {
+				skip = false
+				break
+			}
+		}
+		if skip {
+			result = append(result, v)
+		}
+	}
+
+	return result
+}
+
+// Union creates a slice of unique elements, in order, from all given slices.
+// Play: https://go.dev/play/p/hfXV1iRIZOf
+func SliceUnion[T comparable](slices ...[]T) []T {
+	result := []T{}
+	contain := map[T]struct{}{}
+
+	for _, slice := range slices {
+		for _, item := range slice {
+			if _, ok := contain[item]; !ok {
+				contain[item] = struct{}{}
+				result = append(result, item)
+			}
+		}
+	}
+
+	return result
+}
+
+// Merge all given slices into one slice.
+// Play: https://go.dev/play/p/lbjFp784r9N
+func SliceMerge[T any](slices ...[]T) []T {
+	result := make([]T, 0)
+
+	for _, v := range slices {
+		result = append(result, v...)
+	}
+
+	return result
+}
+
+// ToSlice returns a slices of a variable parameter transformation.
+// Play: https://go.dev/play/p/YzbzVq5kscN
+func ToSlice[T any](items ...T) []T {
+	result := make([]T, len(items))
+	copy(result, items)
+
+	return result
+}
+func ccc() {
+	// slices.
+}
+
+// // Random get a random item of slice, return idx=-1 when slice is empty
+// // Play: https://go.dev/play/p/UzpGQptWppw
+// func Random[T any](slice []T) (val T, idx int) {
+// 	if len(slice) == 0 {
+// 		return val, -1
+// 	}
+
+// 	idx = random.RandInt(0, len(slice))
+// 	return slice[idx], idx
+// }
---
+++ slice_test.go
@@ -0,0 +1,106 @@
+package crab
+
+import (
+	"testing"
+
+	"github.com/serialt/crab/internal"
+)
+
+func TestContainSubSlice(t *testing.T) {
+	t.Parallel()
+
+	assert := internal.NewAssert(t, "TestContainSubSlice")
+	assert.Equal(true, ContainSubSlice([]string{"a", "a", "b", "c"}, []string{"a", "a"}))
+	assert.Equal(false, ContainSubSlice([]string{"a", "a", "b", "c"}, []string{"a", "d"}))
+	assert.Equal(true, ContainSubSlice([]int{1, 2, 3}, []int{1, 2}))
+	assert.Equal(false, ContainSubSlice([]int{1, 2, 3}, []int{0, 1}))
+}
+
+func TestSliceChunk(t *testing.T) {
+	t.Parallel()
+
+	assert := internal.NewAssert(t, "TestChunk")
+
+	arr := []string{"a", "b", "c", "d", "e"}
+
+	assert.Equal([][]string{}, SliceChunk(arr, -1))
+
+	assert.Equal([][]string{}, SliceChunk(arr, 0))
+
+	r1 := [][]string{{"a"}, {"b"}, {"c"}, {"d"}, {"e"}}
+	assert.Equal(r1, SliceChunk(arr, 1))
+
+	r2 := [][]string{{"a", "b"}, {"c", "d"}, {"e"}}
+	assert.Equal(r2, SliceChunk(arr, 2))
+
+	r3 := [][]string{{"a", "b", "c"}, {"d", "e"}}
+	assert.Equal(r3, SliceChunk(arr, 3))
+
+	r4 := [][]string{{"a", "b", "c", "d"}, {"e"}}
+	assert.Equal(r4, SliceChunk(arr, 4))
+
+	r5 := [][]string{{"a", "b", "c", "d", "e"}}
+	assert.Equal(r5, SliceChunk(arr, 5))
+
+	r6 := [][]string{{"a", "b", "c", "d", "e"}}
+	assert.Equal(r6, SliceChunk(arr, 6))
+}
+
+func TestUpdateAt(t *testing.T) {
+	t.Parallel()
+
+	assert := internal.NewAssert(t, "TestUpdateAt")
+
+	assert.Equal([]string{"a", "b", "c"}, SliceUpdateAt([]string{"a", "b", "c"}, -1, "1"))
+	assert.Equal([]string{"1", "b", "c"}, SliceUpdateAt([]string{"a", "b", "c"}, 0, "1"))
+	assert.Equal([]string{"a", "b", "2"}, SliceUpdateAt([]string{"a", "b", "c"}, 2, "2"))
+	assert.Equal([]string{"a", "b", "c"}, SliceUpdateAt([]string{"a", "b", "c"}, 3, "2"))
+}
+
+func TestSliceUnique(t *testing.T) {
+	t.Parallel()
+
+	assert := internal.NewAssert(t, "TestUnique")
+
+	assert.Equal([]int{1, 2, 3}, SliceUnique([]int{1, 2, 2, 3}))
+	assert.Equal([]string{"a", "b", "c"}, SliceUnique([]string{"a", "a", "b", "c"}))
+}
+
+func TestSliceUnion(t *testing.T) {
+	t.Parallel()
+
+	assert := internal.NewAssert(t, "TestUnion")
+
+	s1 := []int{1, 3, 4, 6}
+	s2 := []int{1, 2, 5, 6}
+	s3 := []int{0, 4, 5, 7}
+
+	assert.Equal([]int{1, 3, 4, 6, 2, 5, 0, 7}, SliceUnion(s1, s2, s3))
+	assert.Equal([]int{1, 3, 4, 6, 2, 5}, SliceUnion(s1, s2))
+	assert.Equal([]int{1, 3, 4, 6}, SliceUnion(s1))
+}
+
+func TestMerge(t *testing.T) {
+	t.Parallel()
+
+	assert := internal.NewAssert(t, "TestMerge")
+
+	s1 := []int{1, 2, 3, 4}
+	s2 := []int{2, 3, 4, 5}
+	s3 := []int{4, 5, 6}
+
+	assert.Equal([]int{1, 2, 3, 4, 2, 3, 4, 5, 4, 5, 6}, SliceMerge(s1, s2, s3))
+	assert.Equal([]int{1, 2, 3, 4, 2, 3, 4, 5}, SliceMerge(s1, s2))
+	assert.Equal([]int{2, 3, 4, 5, 4, 5, 6}, SliceMerge(s2, s3))
+}
+
+func TestToSlice(t *testing.T) {
+	t.Parallel()
+
+	assert := internal.NewAssert(t, "TestToSlice")
+
+	str1 := "a"
+	str2 := "b"
+	assert.Equal([]string{"a"}, ToSlice(str1))
+	assert.Equal([]string{"a", "b"}, ToSlice(str1, str2))
+}
