fb2a80a9083dedf835685f1179f0e6be6470079d
Tang <tserialt@gmail.com>
2025-10-20 23:22:16 +0800

    feat: add

--- json.go
+++ json.go
@@ -6,3 +6,8 @@ func Marshal(data any) (str string) {
 	bt, _ := json.Marshal(data)
 	return string(bt)
 }
+
+func MarshalIndent(data any) (str string) {
+	bt, _ := json.MarshalIndent(data, "", "  ")
+	return string(bt)
+}
